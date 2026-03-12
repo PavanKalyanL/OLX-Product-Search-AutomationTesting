6557abef74be0372ca55e731e3d4c1c1e54274b7
Kalyane sree <163311552+Kalyanesree@users.noreply.github.com>
2026-03-12 12:22:34 +0530

    Update ProductDetailsPage.java

--- src/test/java/pages/ProductDetailsPage.java
+++ src/test/java/pages/ProductDetailsPage.java
@@ -1,5 +1,32 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ProductDetailsPage {
+
+WebDriver driver;
+
+By title = By.xpath("//h1");
+
+By price = By.xpath("//span[contains(text(),'₹')]");
+
+public ProductDetailsPage(WebDriver driver){
+
+this.driver = driver;
+
+}
+
+public boolean verifyTitle(){
+
+return driver.findElement(title).isDisplayed();
+
+}
+
+public boolean verifyPrice(){
+
+return driver.findElement(price).isDisplayed();
+
+}
 
 }
